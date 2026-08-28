963de6086012d2bf873ebf95d65ac5090674a65b
fsc <florin.schikarski@sonichealthcare.de>
2026-08-28 11:35:08 +0200

    feat: stringUtils isblank and unslash

--- utils/stringUtils/stringUtils.go
+++ utils/stringUtils/stringUtils.go
@@ -1,5 +1,7 @@
 package stringUtils
 
+import "strings"
+
 func StartsWith(str, subStr string) bool {
 	// case: subStr cannot be a sub string of str
 	if len(subStr) > len(str) {
@@ -16,4 +18,32 @@ func EndsWith(str, subStr string) bool {
 	}
 
 	return str[len(str)-len(subStr):] == subStr
+}
+
+// Remove all leading and trailing forward slashes
+func Unslash(str string) string {
+	if len(str) == 0 {
+		return str
+	}
+
+	for {
+		if !StartsWith(str, "/") && !EndsWith(str, "/") {
+			break
+		}
+
+		if StartsWith(str, "/") {
+			str = str[1:]
+		}
+
+		if EndsWith(str, "/") {
+			str = str[:len(str)-1]
+		}
+	}
+
+	return str
+}
+
+// Indicates that [str] is empty or consists only of white space
+func IsBlank(str string) bool {
+	return len(strings.Trim(str, " ")) == 0
 }
--- utils/stringUtils/stringUtils_test.go
+++ utils/stringUtils/stringUtils_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 
-func TestStartsWith_True(t *testing.T) {
+func TestStartsWith_true(t *testing.T) {
 	assertTrue := func(str string, subStrs []string) {
 		for _, subStr := range subStrs {
 			if (!stringUtils.StartsWith(str, subStr)) {
@@ -33,7 +33,7 @@ func TestStartsWith_True(t *testing.T) {
 	});
 }
 
-func TestStartsWith_False(t *testing.T) {
+func TestStartsWith_false(t *testing.T) {
 	assertFalse := func(str string, subStrs []string) {
 		for _, subStr := range subStrs {
 			if (stringUtils.StartsWith(str, subStr)) {
@@ -52,7 +52,7 @@ func TestStartsWith_False(t *testing.T) {
 	});
 }
 
-func TestEndsWith_True(t *testing.T) {
+func TestEndsWith_true(t *testing.T) {
 	assertTrue := func(str string, subStrs []string) {
 		for _, subStr := range subStrs {
 			if (!stringUtils.EndsWith(str, subStr)) {
@@ -78,7 +78,7 @@ func TestEndsWith_True(t *testing.T) {
 	});
 }
 
-func TestEndsWith_False(t *testing.T) {
+func TestEndsWith_false(t *testing.T) {
 	assertFalse := func(str string, subStrs []string) {
 		for _, subStr := range subStrs {
 			if (stringUtils.EndsWith(str, subStr)) {
@@ -95,4 +95,87 @@ func TestEndsWith_False(t *testing.T) {
 		"xyz",
 		" ",
 	});
+}
+
+func TestUnslash_shouldNotModify(t *testing.T) {
+	assertNotModified := func(str string) {
+		if unslashedStr := stringUtils.Unslash(str); unslashedStr != str {
+			t.Errorf("Expected '%v' to equal '%v'", str, unslashedStr);
+		}
+	}
+
+	testCases := []string{
+		"",
+		" ",
+		"asdf",
+		"asdf/asdf",
+		"asdf/ ",
+		" /asdf",
+		" / ",
+	}
+
+	for _, testCase := range testCases {
+		assertNotModified(testCase);
+	}
+}
+
+func TestUnslash_shouldModify(t *testing.T) {
+	assertModified := func(str string) {
+		if unslashedStr := stringUtils.Unslash(str); unslashedStr == str {
+			t.Errorf("Expected '%v' not to equal '%v'", str, unslashedStr);
+		}
+	}
+
+	testCases := []string{
+		"/",
+		"/ ",
+		"/asdf",
+		"asdf/",
+		" /",
+		"///asdfasdf///",
+		"////",
+	}
+
+	for _, testCase := range testCases {
+		assertModified(testCase);
+	}
+}
+
+func TestIsBlank_shouldBeTrue(t *testing.T) {
+	assertBlank := func(str string) {
+		if !stringUtils.IsBlank(str) {
+			t.Errorf("Expected '%v' to be blank", str);
+		}
+	}
+
+	testCases := []string{
+		"",
+		" ",
+		"     ",
+	}
+
+	for _, testCase := range testCases {
+		assertBlank(testCase);
+	}
+}
+
+func TestIsBlank_shouldBeFalse(t *testing.T) {
+	assertNotBlank := func(str string) {
+		if stringUtils.IsBlank(str) {
+			t.Errorf("Expected '%v' not to be blank", str);
+		}
+	}
+
+	testCases := []string{
+		"d",
+		"3",
+		"\n",
+		"\r\n",
+		"asdfasfas",
+		" d",
+	}
+
+	for _, testCase := range testCases {
+		assertNotBlank(testCase);
+	}
 }
