ee9010ed029fbaa3c872ae4ebd9885cfaae4f861
flobbe9 <florin.schikarski@outlook.comm>
2026-09-05 20:06:23 +0200

    feat: slicesUtils.filter

--- utils/sliceUtils/sliceUtils.go
+++ utils/sliceUtils/sliceUtils.go
@@ -13,7 +13,7 @@ package sliceUtils
 // [return] new slice containing an equivalent element of type [N] for each element from [s]
 //
 // [panic] if an arg is [nil]
-func Map[S any, N any](s []S, mapperCallback func(element S, index int) N) []N {
+func Map[S, N any](s []S, mapperCallback func(element S, index int) N) []N {
 	if s == nil || mapperCallback == nil {
 		panic("Nil arg");
 	}
@@ -24,4 +24,32 @@ func Map[S any, N any](s []S, mapperCallback func(element S, index int) N) []N {
 	} 
 
 	return newSlice;
+}
+
+// Classic "filter" function.
+//
+// [s] the slice to filter
+//
+// [predicate] the callback used on every element in [s]. Accepts the element of iteration and the index and returns [true]
+// if the element should not be filtered out, else [false].
+//
+// <S> the type of slice
+//
+// [return] new slice containing a subset of [s]
+//
+// [panic] if an arg is [nil]
+func Filter[S any](s []S, predicate func(element S, index int) bool) []S {
+	if s == nil || predicate == nil {
+		panic("Nil arg");
+	}
+
+	filtered := []S{};
+
+	for i, element := range s {
+		if predicate(element, i) {
+			filtered = append(filtered, element);
+		}
+	}
+
+	return filtered;
 }
--- utils/sliceUtils/sliceUtils_test.go
+++ utils/sliceUtils/sliceUtils_test.go
@@ -1,6 +1,7 @@
 package sliceUtils_test
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/flobbe9/go/utils/sliceUtils"
@@ -49,4 +50,82 @@ func TestMap_shouldReturnExpectedSlice(t *testing.T) {
 			t.Errorf("Expected element at index %v to equal '%v' but got '%v'", i, expectedEl, result[i]);
 		}
 	}
+}
+
+
+func TestFilter_shouldPanicIfSliceArgNil(t *testing.T) {
+	defer func() {
+        if r := recover(); r == nil {
+            t.Errorf("Expected panic for nil args")
+        }
+    }();
+
+	sliceUtils.Filter(nil, func(el string, i int) bool {return true});
+}
+
+func TestFilter_shouldPanicIfCallbackArgNil(t *testing.T) {
+	defer func() {
+        if r := recover(); r == nil {
+            t.Errorf("Expected panic for nil args")
+        }
+    }();
+
+	var callback func(el string, i int) bool = nil;
+	sliceUtils.Filter([]string{}, callback);
+}
+
+func TestFilter_shouldReturnEmptySliceIfEmptyArg(t *testing.T) {
+	s := []string{};
+	var callback func(el string, i int) bool = func(el string, i int) bool {return true};
+	result := sliceUtils.Filter(s, callback);
+
+	if len(result) > 0 {
+		t.Errorf("Expected empty result for empty slice arg but got %v", result);
+	}
+}
+
+func TestFilter_shouldReturnEmptySliceIfNoMatch(t *testing.T) {
+	s := []int{ 0, 1, 2 };
+	var callback func(el int, i int) bool = func(el int, i int) bool {
+		return el > 100;
+	};
+	result := sliceUtils.Filter(s, callback);
+
+	if len(result) > 0 {
+		t.Errorf("Expected empty result for arg s %v arg but got %v", s, result);
+	}
+}
+
+func TestFilter_shouldFilterByPredicateTrue(t *testing.T) {
+	s := []int{};
+	expectedResult := []int {};
+	var callback func(el int, i int) bool = nil;
+	
+	assertFiltered := func() {
+		result := sliceUtils.Filter(s, callback);
+		if !slices.Equal(expectedResult, result) {
+			t.Errorf("Expected result %v arg but got %v (arg %v)", expectedResult, result, s);
+		}
+	}
+	
+	s = []int{0, 1, 2};
+	expectedResult = []int {1, 2};
+	callback = func(el int, i int) bool {
+		return el > 0;
+	};
+	assertFiltered();
+
+	s = []int{0, 1, 2};
+	expectedResult = []int {0, 1, 2};
+	callback = func(el int, i int) bool {
+		return el > -1;
+	};
+	assertFiltered();
+	
+	s = []int{0, 1, 2, 3};
+	expectedResult = []int {0, 2};
+	callback = func(el int, i int) bool {
+		return el % 2 == 0;
+	};
+	assertFiltered();
 }
