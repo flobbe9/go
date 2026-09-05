28cb959c03765661d01a95f4c829879f17b7ad97
flobbe9 <florin.schikarski@outlook.comm>
2026-09-05 10:53:26 +0200

    feat: testUtils

---
+++ utils/testUtils/testUtils.go
@@ -0,0 +1,10 @@
+package testutils
+
+import "testing"
+
+// Fail the test with [t.Error] if [err] is not [nil]. Do nothing otherwise
+func ErrorTestFail(err error, t *testing.T) {
+	if err != nil {
+		t.Error(err);
+	}
+}
